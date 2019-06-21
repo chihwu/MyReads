c03aed023a64744a4fdf1bbfb76ea08d83e8a1a9
Chihyung Wu <chihwu@bu.edu>
2019-06-21 19:16:20 -0400

    add structure and logics

---
+++ build/precache-manifest.f9b6229973c7098db93e8f2073b6e73c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cbbb2966bd3569a4805",
+    "url": "/static/css/main.cc65ca93.chunk.css"
+  },
+  {
+    "revision": "4cbbb2966bd3569a4805",
+    "url": "/static/js/main.4cbbb296.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "c04472f5ee407069585f85273685164d",
+    "url": "/index.html"
+  }
+];
---
+++ src/DivBookShelves.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import DivBookShelf from './DivBookShelf'
+import { Link } from 'react-router-dom'
+
+class DivBookShelves extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			current_reading: [],
+			want_to_read: [],
+			read: []
+		};
+	}
+
+	componentDidMount() {
+		let current_reading = [];
+		let want_to_read = [];
+		let read = [];
+
+		this.props.books.forEach((book) => {
+			if (book.shelf === 'Currently Reading') {
+				current_reading.push(book);
+			} else if (book.shelf === 'Want to Read') {
+				want_to_read.push(book);
+			} else if (book.shelf === 'Read') {
+				read.push(book);
+			}
+		});
+
+		this.setState({
+			current_reading: current_reading,
+			want_to_read: want_to_read,
+			read: read
+		});
+	}
+
+	render() {
+		
+
+		return (
+			<div className="list-books">
+	            <div className="list-books-title">
+	              <h1>MyReads</h1>
+	            </div>
+	            <div className="list-books-content">
+	              <div>
+	                <DivBookShelf title='Currently Reading' books={ this.state.current_reading } />
+	                <DivBookShelf title='Want to Read' books={ this.state.want_to_read } />
+	                <DivBookShelf title='Read' books={ this.state.read } />
+	              </div>
+	            </div>
+	            <div className="open-search">
+	            	<Link to="/search">Add a book</Link>
+	            </div>
+          	</div>
+		);
+	}
+
+}
+
+export default DivBookShelves
+
+
+
